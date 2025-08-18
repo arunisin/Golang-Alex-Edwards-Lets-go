753022ab9f05b9d14f0d35b91fe4a093d85b3b6f
arunisin <arunisin.4u@gmail.com>
2025-08-18 19:04:44 +0530

    added some queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"learning/pkg/models"
 	"net/http"
 	"strconv"
-	"text/template"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -12,36 +12,55 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
-
-	files := []string{
-		"ui/html/home.page.tmpl",
-		"ui/html/base.layout.tmpl",
-		"ui/html/footer.partial.tmpl",
-	}
-	ts, err := template.ParseFiles(files...)
-
+	s, err := app.snippets.Latest()
 	if err != nil {
-		app.errorlog.Println(err.Error())
 		app.serverError(w, err)
 		return
 	}
-
-	err = ts.Execute(w, nil)
-
-	if err != nil {
-		app.errorlog.Println(err.Error())
-		app.serverError(w, err)
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
+
+	// files := []string{
+	// 	"ui/html/home.page.tmpl",
+	// 	"ui/html/base.layout.tmpl",
+	// 	"ui/html/footer.partial.tmpl",
+	// }
+	// ts, err := template.ParseFiles(files...)
+
+	// if err != nil {
+	// 	app.errorlog.Println(err.Error())
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// err = ts.Execute(w, nil)
+
+	// if err != nil {
+	// 	app.errorlog.Println(err.Error())
+	// 	app.serverError(w, err)
+	// }
 }
 
 // Add a showSnippet handler function.
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	fmt.Printf("%v is the id", id)
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
 	}
-	fmt.Fprintf(w, "request id is %v", id)
+	s, err := app.snippets.Get(id)
+	if err == models.ErrNoRecord {
+		fmt.Printf("no row for %v id", id)
+		app.notFound(w)
+		return
+	} else if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%v", s)
 }
 
 // Add a createSnippet handler function.
@@ -51,5 +70,17 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create a new snippet..."))
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi"
+	expires := "7"
+	// Pass the data to the SnippetModel.Insert() method, receiving the
+	// ID of the new record back.
+	id, err := app.snippets.Insert(title, content, expires)
+
+	if err != nil {
+		app.serverError(w, err)
+	}
+	// Redirect the user to the relevant page for the snippet.
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,35 +1,58 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"learning/pkg/models/mysql"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql" // New import
 )
 
 type application struct {
 	errorlog *log.Logger
 	infolog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-	app := &application{
-		errorlog: errorLog, infolog: infoLog,
-	}
 	addr := flag.String("addr", ":4000", "HTTP network port")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL DSN")
 	flag.Parse()
 
 	infoLog.Println("Starting server")
 	log.Printf("starting server on %s", *addr)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	app := &application{
+		errorlog: errorLog, infolog: infoLog, snippets: &mysql.SnippetModel{DB: db},
+	}
+	defer db.Close()
 
 	srv := http.Server{
 		Addr:     *addr,
 		Handler:  app.routes(),
 		ErrorLog: errorLog,
 	}
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
